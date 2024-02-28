b166ec0c46adab6df4dff89ac92674a27b31d0ac
Sirampy <alex.ch4.cl@gmail.com>
2024-02-28 23:28:34 +0000

    =IT COMPILES

---
+++ server/target/debug/deps/server-a46404ee4178d2cc.d
@@ -0,0 +1,5 @@
+/home/alex/Documents/yr_2/infoproc-cw/server/target/debug/deps/server-a46404ee4178d2cc: src/main.rs
+
+/home/alex/Documents/yr_2/infoproc-cw/server/target/debug/deps/server-a46404ee4178d2cc.d: src/main.rs
+
+src/main.rs:
---
+++ server/target/debug/server.d
@@ -0,0 +1,1 @@
+/home/alex/Documents/yr_2/infoproc-cw/server/target/debug/server: /home/alex/Documents/yr_2/infoproc-cw/server/src/main.rs
